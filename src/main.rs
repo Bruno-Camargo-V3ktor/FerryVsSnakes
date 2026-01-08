7817b92a2e3347d9dcee662becbee6ddc41f2baf
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-08 14:28:05 -0300

    core: setup folders

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
+mod components;
+mod scenes;
+
 fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(WorldInspectorPlugin::default())
         .add_plugins(EguiPlugin::default())
+        .add_plugins(WorldInspectorPlugin::default())
         .run()
 }
